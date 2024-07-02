afc3396faf6fe98f728cbad4b1eaf817fc44d025
Takuya Eguchi <seal2501@gmail.com>
2024-07-02 16:27:18 +0900

    wip: とちゅうまで。うまくいかない

--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,39 @@
+import { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { FlatCompat } from '@eslint/eslintrc';
 import tsEslint from "typescript-eslint";
 import tsEslintParser from "@typescript-eslint/parser";
 import vueEslintParser from 'vue-eslint-parser';
 import pluginVue from "eslint-plugin-vue";
+import eslintPluginImport from 'eslint-plugin-import';
+
+const eslintrc = new FlatCompat({
+  baseDirectory: dirname(fileURLToPath(import.meta.url)),
+});
 
 /**
- * ESLint config for TypeScript
+ * 全体に適応するカスタムルール
  * @type {import('eslint').Linter.FlatConfig}
  */
-const commonConfig = {
+const baseConfig = {
+  plugins: {
+    import: eslintPluginImport,
+  },
+  ignores: ['**/dist/**/*', '**/node_modules/**/*'],
 };
 
 /**
- * ESLint config for TypeScript
+ * 全体に適応するカスタムルール
+ * @type {import('eslint').Linter.FlatConfig}
+ */
+const commonRules = {
+  rules: {
+    // '@typescript-eslint/no-unused-vars': ['off'],
+  },
+};
+
+/**
+ * Vue ファイルに適応するカスタムルール
  * @type {import('eslint').Linter.FlatConfig}
  */
 const vueConfig = {
@@ -29,23 +51,30 @@ const vueConfig = {
 };
 
 /**
- * ESLint config for TypeScript
+ * TS/JS ファイルに適応するカスタムルール
  * @type {import('eslint').Linter.FlatConfig}
  */
 const tsConfig = {
-  files: ["**/*.ts"],
+  files: ["**/*.{js,ts}"],
   languageOptions: {
     parserOptions: {
       parser: tsEslintParser,
       sourceType: "module",
     },
   },
+  rules: {
+    // '@typescript-eslint/no-unused-vars': ['off'],
+  },
 };
 
+const test = eslintrc.extends('@vue/eslint-config-airbnb');
+console.log({ test });
 export default [
+  baseConfig,
   ...tsEslint.configs.recommended,
   ...pluginVue.configs['flat/recommended'],
-  commonConfig,
+  ...eslintrc.extends('@vue/eslint-config-airbnb'),
+  // commonConfig,
   vueConfig,
   tsConfig,
 ];
